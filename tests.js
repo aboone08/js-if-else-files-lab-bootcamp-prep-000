6d72cf3b690f7bf1ed9bbf0e2f67537963c7416b
andranique.boone@gmail.com <andranique.boone@gmail.com>
2018-03-10 06:43:25 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 var index = require("./index.js")
 
-if (index.name === "Bob") {
+if (index.name === "name") {
     console.log("The name is correct");
 } else {
     console.log("Expected: Susan, Received: "+index.name);
